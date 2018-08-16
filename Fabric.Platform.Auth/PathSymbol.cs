75a61c10ed7b7ed03af029d19dc77bd8ad2616b4
Jorden Lowe <jorden.lowe@healthcatalyst.com>
2018-08-16 13:34:07 -0400

    Added logic to allow wildcards at the end of paths

---
+++ Fabric.Platform.Auth/PathSymbol.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fabric.Platform.Auth
+{
+    internal static class PathSymbol
+    {
+        public const char AnyNumberOfCharacters = '*';
+    }
+}
